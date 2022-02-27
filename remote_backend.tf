30ca5a8f139b63a72da680039b264f82a0845864
Aya Hara <you@example.com>
2022-02-27 21:31:13 +0000

    edit remote

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,17 +1,9 @@
 terraform {
   backend "remote" {
-<<<<<<< HEAD
-    hostname = "app.terraform.io"
-=======
     hostname     = "app.terraform.io"
->>>>>>> 25612992497241ca7b7c089d5c150ca604f099d8
     organization = "HARA-training"
     workspaces {
       name = "hashicat-aws"
     }
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 25612992497241ca7b7c089d5c150ca604f099d8
